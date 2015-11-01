723a2aea21b0b289d4382d352b883b05a4feecc6
LukasSouza <lukas_santos15@hotmail.com>
2015-11-01 00:13:55 -0300

    Mudança no projeto
    Após a greve, decidimos mudar a tecnologia ultilizada, de Java para PHP

--- Código Fonte/Progessao/WebContent/view/js/url_index.js
+++
@@ -1,5 +0,0 @@
-
-window.onload = function(){	
-	if(document.location.href!="http://localhost:8080/progressao/view/index.html")
-		document.location.href="http://localhost:8080/progressao/view/index.html";
-};
